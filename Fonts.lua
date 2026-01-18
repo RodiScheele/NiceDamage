d046a5b4396a0536b719e8a83ab054bad8015337
Azaiko <azaiko22@gmail.com>
2026-01-18 14:48:53 +0100

    add extra fonts

--- Fonts.lua
+++ Fonts.lua
@@ -2,9 +2,18 @@ local _, addon = ...
 local LSM = LibStub("LibSharedMedia-3.0")
 
 -- Define custom font files located in \NiceDamage\fonts\
+-- Add fonts here if you want to add custom fonts ["Font Name"] = "fontfile.ttf"
 local myFonts = {
     ["Pepsi Modern"] = "pepsi_modern.ttf",
-    ["Pepsi Cursive"] = "pepsi_cursive.ttf"
+    ["Pepsi Cursive"] = "pepsi_cursive.ttf",
+    ["Bangers"] = "Bangers.ttf",
+    ["Pf Tempesta Seven"] = "pf_tempesta_seven.ttf",
+    ["Prototype"] = "Prototype.ttf",
+    ["Expressway"] = "Expressway.ttf",
+    ["Roboto Bold"] = "Roboto-Bold.ttf",
+    ["Big Noodle Titling"] = "bignoodletitling.ttf",
+    ["Die Die Die"] = "DIEDIEDI.ttf",
+    ["LifeCraft"] = "LifeCraft_Font.ttf"
 }
 
 function addon:RegisterFonts()
